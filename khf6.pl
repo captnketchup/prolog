6290dfc88305be0cd54bbe5f8c24ec7d78e73d1a
captnketchup <74988054+captnketchup@users.noreply.github.com>
2022-11-14 11:32:19 +0100

    Finished khf6

--- khf6.pl
+++ khf6.pl
@@ -48,61 +48,28 @@ fa_szamlalas(Fak, Osszegfeltetel, Iranylistak, BiztosFak, EsetlegesFak) :-
         esetleg_teljesul(Fa, Osszegfeltetel, Iranylista)
         ), EsetlegesFak).
 
-% fa_szamlalas([], _, [], [], []) :- 
-%     write_ln("rekurzio vege")
-%     .
 
-% fa_szamlalas([FaHead|FaTail], Osszegfeltetel, [IlHead|IlTail], [BHead|BTail], [EHead|ETail]) :-
-    % (
-    %         biztos_teljesul(FaHead, Osszegfeltetel, IlHead) ->         
-    %             (
-    %                 write_ln('biztos teljesul'),
-    %                 BHead = FaHead,
-    %                 fa_szamlalas(FaTail, Osszegfeltetel, IlTail, BTail, [EHead|ETail]))
-    %     ;   esetleg_teljesul(FaHead, Osszegfeltetel, IlHead) -> 
-    %             ( 
-    %                 write_ln('esetleg teljesul'),
-    %                 EHead = FaHead,
-    %                 fa_szamlalas(FaTail, Osszegfeltetel, IlTail, [BHead|BTail], ETail))
-    %     ;   (
-    %             write_ln('nem teljesul'),
-    %             fa_szamlalas(FaTail, Osszegfeltetel, IlTail, [BHead|BTail], [EHead|ETail])
-    %         )
-    % ).
-
-% szukiti esetlegesen a fakat
-
-esetleg_szukites([], _, _, _, []).
-esetleg_szukites([F|Fs], sor(I, Db), [IL0|ILs0], EFs, [IL|ILs]) :-
-    (   member(F, EFs) ->  esetlegesbol_biztos(F, sor(I, Db), IL0, IL)
-    ;   IL = IL0
+% #esetleges fa == #biztos fa, akkor minden esetlegesből biztos fát csinálunk úgy, hogy kitöröljük
+% az esetlegesek alternatív irányait (ii. eset)
+esetleg_szukites([], _, _, _, []) :- !.
+esetleg_szukites([FakHead|FakTail], sor(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
+    (
+        % megnézni esetleges fa-e, ha igen biztost csinálni belőle
+        member(FakHead, EsetlegesFak) -> esetlegesbol_biztos(FakHead, sor(I, Db), IranyHead, SzukHead)
+    ;   SzukHead = IranyHead
     ),
-    esetleg_szukites(Fs, sor(I, Db), ILs0, EFs, ILs).
-esetleg_szukites([F|Fs], oszl(J, Db), [IL0|ILs0], EFs, [IL|ILs]) :-
-    (   member(F, EFs) ->  esetlegesbol_biztos(F, oszl(J, Db), IL0, IL)
-    ;   IL = IL0
+    esetleg_szukites(FakTail, sor(I, Db), IranyTail, EsetlegesFak, SzukTail).
+
+
+esetleg_szukites([FakHead|FakTail], oszl(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
+    (
+        % megnézni esetleges fa-e, ha igen biztost csinálni belőle
+    member(FakHead, EsetlegesFak) -> esetlegesbol_biztos(FakHead, oszl(I, Db), IranyHead, SzukHead)
+    ;   SzukHead = IranyHead
     ),
-    esetleg_szukites(Fs, oszl(J, Db), ILs0, EFs, ILs).
+    esetleg_szukites(FakTail, oszl(I, Db), IranyTail, EsetlegesFak, SzukTail).
 
-
-% esetleg_szukites([], _, _, _, []) :- !.
-% esetleg_szukites([FakHead|FakTail], sor(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
-%     (
-%         % megnézni esetleges fa-e, ha igen biztost csinálni belőle
-%         member(FakHead, EsetlegesFak) -> esetlegesbol_biztos(FakHead, sor(I, Db), IranyHead, SzukHead)
-%     ;   SzukHead = IranyHead
-%     ),
-%     esetleg_szukites(FakTail, sor(I, Db), IranyTail, EsetlegesFak, SzukTail).
-
-% % szukiti esetlegesen a fakat
-% esetleg_szukites([FakHead|FakTail], oszl(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
-%     (
-%         % megnézni esetleges fa-e, ha igen biztost csinálni belőle
-%     member(FakHead, EsetlegesFak) -> esetlegesbol_biztos(FakHead, oszl(I, Db), IranyHead, SzukHead)
-%     ;   SzukHead = IranyHead
-%     ),
-%     esetleg_szukites(FakTail, oszl(I, Db), IranyTail, EsetlegesFak, SzukTail).
-
+% hogyha #biztos fa == #összes fa akkor ki kell törölni közülük az esetlegeseket (iii. eset)
 biztos_szukites([], _, _, _, []) :- !.
 biztos_szukites([FakHead|FakTail], oszl(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
     (
@@ -118,6 +85,7 @@ biztos_szukites([FakHead|FakTail], sor(I, Db), [IranyHead|IranyTail], EsetlegesF
     ),
     biztos_szukites(FakTail, sor(I, Db), IranyTail, EsetlegesFak, SzukTail).
 
+% esetleges fákból biztos fákat csinálunk úgy, hogy elhagyjuk az egyéb irányokat
 esetlegesbol_biztos(FaX-_, sor(I, _), Iranylista, SzukLista) :-
     (
         FaX =:= I - 1 -> SzukLista = [s]   % ha a fa feljebb van akkor a sátor s
@@ -131,6 +99,7 @@ esetlegesbol_biztos(_-FaY, oszl(I,_), Iranylista, SzukLista) :-
       ; FaY == I -> delete(Iranylista, e, SzukLista2), delete(SzukLista2, w, SzukLista) % hogyha a fa ugyanabban a sorban törlöm az w-t és e-t
     ).
 
+% #biztos fák == Db, kitöröljük az éppen Osszegfeltetelbe eső esetleges irányokat
 esetleges_torles(FaX-_, sor(I, _), Iranylista, SzukLista) :-
     (
         FaX =:= I - 1 -> delete(Iranylista, s, SzukLista)   % ha a fa feljebb van akkor delete
@@ -145,28 +114,30 @@ esetleges_torles(_-FaY, oszl(I, _), Iranylista, SzukLista) :-
       ; FaY == I -> delete(Iranylista, s, SzukLista2), delete(SzukLista2, n, SzukLista) 
     ).
 
-% biztos_teljesul(_, _, []) :- !.
-% biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
-%     Osszegfeltetel = sor(I,_),
-%     length([IranyHead|IranyTail], L),
-%     L < 3,
-%     get_tent(Fa, IranyHead, X-_),
-%     (
-%         X \= I;
-%     biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
-%     ).
+% egy adott fára biztos-e a feltételre nézve
+biztos_teljesul(_, _, []) :- !.
+biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
+    Osszegfeltetel = sor(I,_),
+    length([IranyHead|IranyTail], L),
+    L < 3,
+    get_tent(Fa, IranyHead, X-_),
+    (
+        X \= I;
+    biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
+    ).
 
-% biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
-%     Osszegfeltetel = oszl(I,_),
-%     length([IranyHead|IranyTail], L),
-%     L < 3,
-%     get_tent(Fa, IranyHead, _-Y),
-%     (
-%         Y \= I;
-%     biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
-%     ).
+biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
+    Osszegfeltetel = oszl(I,_),
+    length([IranyHead|IranyTail], L),
+    L < 3,
+    get_tent(Fa, IranyHead, _-Y),
+    (
+        Y \= I;
+    biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
+    ).
 
-
+% egy adott fa esetleges-e a feltételre nézve
+% picit átírni XD
 esetleg_teljesul(_-Fy, oszl(J, _), IL0) :-
     (   Fy == J ->      (member(e, IL0), !; member(w, IL0)), 
                         (member(n, IL0), !; member(s, IL0))
@@ -179,17 +150,6 @@ esetleg_teljesul(Fx-_, sor(I, _), IL0) :-
     ;   Fx =:= I - 1 -> member(s, IL0), length(IL0, Len), Len > 1
     ;   Fx =:= I + 1 -> member(n, IL0), length(IL0, Len), Len > 1
     ).
-
-biztos_teljesul(_-Fy, oszl(J, _), IL0) :-
-(   Fy == J ->       \+ member(e, IL0), \+ member(w, IL0)
-;   Fy =:= J - 1 -> IL0 == [e]
-;   Fy =:= J + 1 -> IL0 == [w]
-).
-biztos_teljesul(Fx-_, sor(I, _), IL0) :-
-(   Fx == I ->       \+ member(n, IL0), \+ member(s, IL0)
-;   Fx =:= I - 1 -> IL0 == [s]
-;   Fx =:= I + 1 -> IL0 == [n]
-).
 
 % esetleg_teljesul(_, _, []) :- false.
 % esetleg_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :- 
@@ -218,7 +178,7 @@ biztos_teljesul(Fx-_, sor(I, _), IL0) :-
 %         (esetleg_teljesul(Fa, Osszegfeltetel, IranyTail))
 %     ).
 
-
+% sátor pozíciója egy fa helyzetéből és irányából
 get_tent(X-Y, Direction, Tent) :-
     (
         Direction == n -> X1 is X-1, Tent = X1-Y
