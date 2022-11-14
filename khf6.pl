fe101f2ac1d0ed58f9e74fa587f3854036c1343c
captnketchup <74988054+captnketchup@users.noreply.github.com>
2022-11-14 11:22:59 +0100

    Gyuri <3

--- khf6.pl
+++ khf6.pl
@@ -3,77 +3,220 @@
 % ILs: kimeneti (csökkentett) változó
 % pattern matching sorra
 osszeg_szukites(Fs, Osszegfeltetel, ILs0, ILs) :-
-    Osszegfeltetel = sor(I,Db),
-    fa_szamlalas(Fs, Osszegfeltetel, ILs0, B, E)
+    Osszegfeltetel = sor(_,Db),
+    fa_szamlalas(Fs, Osszegfeltetel, ILs0, Bs, Es),
+    write_ln(Bs),
+    write_ln(Es),
+    length(Bs, B),
+    length(Es, E),
+    (
+        B + E < Db -> ILs = []
+    ;   B + E =:= Db -> esetleg_szukites(Fs, Osszegfeltetel, ILs0, Es, ILs)
+    ;   B == Db -> biztos_szukites(Fs, Osszegfeltetel, ILs0, Es, ILs)
+    ;   B > Db -> ILs = []
+    )
     .
 
 % pattern matching oszlopra
 osszeg_szukites(Fs, Osszegfeltetel, ILs0, ILs) :-
-    Osszegfeltetel = oszl(I,Db),
-    fa_szamlalas(Fs, Osszegfeltetel, ILs0, B, E)
+    Osszegfeltetel = oszl(_,Db),
+    fa_szamlalas(Fs, Osszegfeltetel, ILs0, Bs, Es),
+    write_ln(Bs),
+    write_ln(Es),
+    length(Bs, B),
+    length(Es, E),
+    (
+        B + E < Db -> ILs = []
+    ;   B + E =:= Db -> esetleg_szukites(Fs, Osszegfeltetel, ILs0, Es, ILs)
+    ;   B == Db -> biztos_szukites(Fs, Osszegfeltetel, ILs0, Es, ILs)
+    ;   B > Db -> ILs = []
+    )
     .
 
-fa_szamlalas([], _, [], 0, 0).
-fa_szamlalas([FaHead|FaTail], Osszegfeltetel, [IlHead|IlTail], B, E) :-
-    (
-        biztos_teljesul(FaHead, Osszegfeltetel, IlHead),
-        fa_szamlalas(FaTail, Osszegfeltetel, IlTail, B1, E),
-        B is B1 + 1
-        );
-    (
-        esetleg_teljesul(FaHead, Osszegfeltetel, IlHead),
-        fa_szamlalas(FaTail, Osszegfeltetel, IlTail, B, E1),
-        E is E1 + 1
-    );
-    fa_szamlalas(FaTail, Osszegfeltetel, IlTail, B, E).
+% megkeresni minden biztosan és esetlegesen teljesülő fát
+fa_szamlalas(Fak, Osszegfeltetel, Iranylistak, BiztosFak, EsetlegesFak) :-
+    findall(Fa, (
+        member(Fa, Fak),
+        nth0(Index, Fak, Fa),
+        nth0(Index, Iranylistak, Iranylista),
+        biztos_teljesul(Fa, Osszegfeltetel, Iranylista)
+        ), BiztosFak),
+    findall(Fa, (
+        member(Fa, Fak),
+        nth0(Index, Fak, Fa),
+        nth0(Index, Iranylistak, Iranylista),
+        esetleg_teljesul(Fa, Osszegfeltetel, Iranylista)
+        ), EsetlegesFak).
 
-biztos_teljesul(_, _, []) :- !.
-biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
-    Osszegfeltetel = sor(I,_),
-    length([IranyHead|IranyTail], L),
-    L < 3,
-    get_tent(Fa, IranyHead, X-_),
+% fa_szamlalas([], _, [], [], []) :- 
+%     write_ln("rekurzio vege")
+%     .
+
+% fa_szamlalas([FaHead|FaTail], Osszegfeltetel, [IlHead|IlTail], [BHead|BTail], [EHead|ETail]) :-
+    % (
+    %         biztos_teljesul(FaHead, Osszegfeltetel, IlHead) ->         
+    %             (
+    %                 write_ln('biztos teljesul'),
+    %                 BHead = FaHead,
+    %                 fa_szamlalas(FaTail, Osszegfeltetel, IlTail, BTail, [EHead|ETail]))
+    %     ;   esetleg_teljesul(FaHead, Osszegfeltetel, IlHead) -> 
+    %             ( 
+    %                 write_ln('esetleg teljesul'),
+    %                 EHead = FaHead,
+    %                 fa_szamlalas(FaTail, Osszegfeltetel, IlTail, [BHead|BTail], ETail))
+    %     ;   (
+    %             write_ln('nem teljesul'),
+    %             fa_szamlalas(FaTail, Osszegfeltetel, IlTail, [BHead|BTail], [EHead|ETail])
+    %         )
+    % ).
+
+% szukiti esetlegesen a fakat
+
+esetleg_szukites([], _, _, _, []).
+esetleg_szukites([F|Fs], sor(I, Db), [IL0|ILs0], EFs, [IL|ILs]) :-
+    (   member(F, EFs) ->  esetlegesbol_biztos(F, sor(I, Db), IL0, IL)
+    ;   IL = IL0
+    ),
+    esetleg_szukites(Fs, sor(I, Db), ILs0, EFs, ILs).
+esetleg_szukites([F|Fs], oszl(J, Db), [IL0|ILs0], EFs, [IL|ILs]) :-
+    (   member(F, EFs) ->  esetlegesbol_biztos(F, oszl(J, Db), IL0, IL)
+    ;   IL = IL0
+    ),
+    esetleg_szukites(Fs, oszl(J, Db), ILs0, EFs, ILs).
+
+
+% esetleg_szukites([], _, _, _, []) :- !.
+% esetleg_szukites([FakHead|FakTail], sor(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
+%     (
+%         % megnézni esetleges fa-e, ha igen biztost csinálni belőle
+%         member(FakHead, EsetlegesFak) -> esetlegesbol_biztos(FakHead, sor(I, Db), IranyHead, SzukHead)
+%     ;   SzukHead = IranyHead
+%     ),
+%     esetleg_szukites(FakTail, sor(I, Db), IranyTail, EsetlegesFak, SzukTail).
+
+% % szukiti esetlegesen a fakat
+% esetleg_szukites([FakHead|FakTail], oszl(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
+%     (
+%         % megnézni esetleges fa-e, ha igen biztost csinálni belőle
+%     member(FakHead, EsetlegesFak) -> esetlegesbol_biztos(FakHead, oszl(I, Db), IranyHead, SzukHead)
+%     ;   SzukHead = IranyHead
+%     ),
+%     esetleg_szukites(FakTail, oszl(I, Db), IranyTail, EsetlegesFak, SzukTail).
+
+biztos_szukites([], _, _, _, []) :- !.
+biztos_szukites([FakHead|FakTail], oszl(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
     (
-        X \= I;
-    biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
+        member(FakHead, EsetlegesFak) -> esetleges_torles(FakHead, oszl(I, Db), IranyHead, SzukHead)
+        ;   SzukHead = IranyHead
+    ),
+    biztos_szukites(FakTail, oszl(I, Db), IranyTail, EsetlegesFak, SzukTail).
+
+biztos_szukites([FakHead|FakTail], sor(I, Db), [IranyHead|IranyTail], EsetlegesFak, [SzukHead|SzukTail]) :-
+    (
+    member(FakHead, EsetlegesFak) -> esetleges_torles(FakHead, sor(I, Db), IranyHead, SzukHead)
+    ;   SzukHead = IranyHead
+    ),
+    biztos_szukites(FakTail, sor(I, Db), IranyTail, EsetlegesFak, SzukTail).
+
+esetlegesbol_biztos(FaX-_, sor(I, _), Iranylista, SzukLista) :-
+    (
+        FaX =:= I - 1 -> SzukLista = [s]   % ha a fa feljebb van akkor a sátor s
+      ; FaX =:= I + 1 -> SzukLista = [n]   % ha a fa lejjebb van akkor a sátor n
+      ; FaX == I -> delete(Iranylista, s, SzukLista2), delete(SzukLista2, n, SzukLista) % hogyha a fa ugyanabban a sorban törlöm az s-t és n-t
+    ).
+esetlegesbol_biztos(_-FaY, oszl(I,_), Iranylista, SzukLista) :-
+    (
+        FaY =:= I - 1 -> SzukLista = [e]   % ha a fa balrabb van akkor e
+      ; FaY =:= I + 1 -> SzukLista = [w]   % ha a fa jobbrabb van akkor w
+      ; FaY == I -> delete(Iranylista, e, SzukLista2), delete(SzukLista2, w, SzukLista) % hogyha a fa ugyanabban a sorban törlöm az w-t és e-t
     ).
 
-biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
-    Osszegfeltetel = oszl(I,_),
-    length([IranyHead|IranyTail], L),
-    L < 3,
-    get_tent(Fa, IranyHead, _-Y),
+esetleges_torles(FaX-_, sor(I, _), Iranylista, SzukLista) :-
     (
-        Y \= I;
-    biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
+        FaX =:= I - 1 -> delete(Iranylista, s, SzukLista)   % ha a fa feljebb van akkor delete
+      ; FaX =:= I + 1 -> delete(Iranylista, n, SzukLista)   % ha a fa lejjebb van akkor delete
+      ; FaX == I -> delete(Iranylista, e, SzukLista2), delete(SzukLista2, w, SzukLista) % hogyha a fa ugyanabban a sorban törlöm az e-t és w-t
     ).
 
-esetleg_teljesul(_, _, []) :- false.
-esetleg_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :- 
-    Osszegfeltetel = sor(I,_),
+esetleges_torles(_-FaY, oszl(I, _), Iranylista, SzukLista) :-
     (
-        (
-        get_tent(Fa,IranyHead, X-_),
-        X == I,
-        % ezt az elemet kell bent hagyni a listában    
-        )
-        ;
-        % a többi elemet removeolni
-    (esetleg_teljesul(Fa, Osszegfeltetel, IranyTail))
+        FaY =:= I - 1 -> delete(Iranylista, e, SzukLista)   
+      ; FaY =:= I + 1 -> delete(Iranylista, w, SzukLista)   
+      ; FaY == I -> delete(Iranylista, s, SzukLista2), delete(SzukLista2, n, SzukLista) 
     ).
 
-esetleg_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :- 
-    Osszegfeltetel = oszl(I,_),
-    (
-        (
-        get_tent(Fa, IranyHead, _-Y),
-        Y == I,
-        % ezt az elemet kell bent hagyni a listában
-        )
-        ;
-        % a többi elemet removeolni
-        (esetleg_teljesul(Fa, Osszegfeltetel, IranyTail))
+% biztos_teljesul(_, _, []) :- !.
+% biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
+%     Osszegfeltetel = sor(I,_),
+%     length([IranyHead|IranyTail], L),
+%     L < 3,
+%     get_tent(Fa, IranyHead, X-_),
+%     (
+%         X \= I;
+%     biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
+%     ).
+
+% biztos_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :-
+%     Osszegfeltetel = oszl(I,_),
+%     length([IranyHead|IranyTail], L),
+%     L < 3,
+%     get_tent(Fa, IranyHead, _-Y),
+%     (
+%         Y \= I;
+%     biztos_teljesul(Fa, Osszegfeltetel, IranyTail)
+%     ).
+
+
+esetleg_teljesul(_-Fy, oszl(J, _), IL0) :-
+    (   Fy == J ->      (member(e, IL0), !; member(w, IL0)), 
+                        (member(n, IL0), !; member(s, IL0))
+    ;   Fy =:= J - 1 -> member(e, IL0), length(IL0, Len), Len > 1
+    ;   Fy =:= J + 1 -> member(w, IL0), length(IL0, Len), Len > 1
     ).
+esetleg_teljesul(Fx-_, sor(I, _), IL0) :-
+    (   Fx == I ->  (member(e, IL0), !; member(w, IL0)), 
+                    (member(n, IL0), !; member(s, IL0))
+    ;   Fx =:= I - 1 -> member(s, IL0), length(IL0, Len), Len > 1
+    ;   Fx =:= I + 1 -> member(n, IL0), length(IL0, Len), Len > 1
+    ).
+
+biztos_teljesul(_-Fy, oszl(J, _), IL0) :-
+(   Fy == J ->       \+ member(e, IL0), \+ member(w, IL0)
+;   Fy =:= J - 1 -> IL0 == [e]
+;   Fy =:= J + 1 -> IL0 == [w]
+).
+biztos_teljesul(Fx-_, sor(I, _), IL0) :-
+(   Fx == I ->       \+ member(n, IL0), \+ member(s, IL0)
+;   Fx =:= I - 1 -> IL0 == [s]
+;   Fx =:= I + 1 -> IL0 == [n]
+).
+
+% esetleg_teljesul(_, _, []) :- false.
+% esetleg_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :- 
+%     Osszegfeltetel = sor(I,_),
+%     (
+%         (
+%         get_tent(Fa,IranyHead, X-_),
+%         X == I
+%         % ezt az elemet kell bent hagyni a listában    
+%         ),!
+%         ;
+%         % a többi elemet removeolni
+%         (esetleg_teljesul(Fa, Osszegfeltetel, IranyTail))
+%     ).
+
+% esetleg_teljesul(Fa, Osszegfeltetel, [IranyHead|IranyTail]) :- 
+%     Osszegfeltetel = oszl(I,_),
+%     (
+%         (
+%         get_tent(Fa, IranyHead, _-Y),
+%         Y == I
+%         % ezt az elemet kell bent hagyni a listában
+%         ),!
+%         ;
+%         % a többi elemet removeolni
+%         (esetleg_teljesul(Fa, Osszegfeltetel, IranyTail))
+%     ).
 
 
 get_tent(X-Y, Direction, Tent) :-
